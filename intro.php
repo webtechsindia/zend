c9cc55bf5c33a369fa78b3c25f1888931a4694dc
hari office <harikrishna266@gmail.com>
2014-07-23 14:36:07 +0530

    classes intro

---
+++ intro.php
@@ -0,0 +1,44 @@
+<?php
+/*
+	main features of php5 oops are as bellow
+
+	1)visibility
+	2)abstract
+	3)final
+	4)magic methods
+	5)interfaces
+	6)cloning
+	7)typehinting
+
+
+
+	//class 
+
+	//variables are called propperties
+	//functions are called methods
+
+	//classes names can nbe /^[azAZ]/
+
+	/$this is called pseudo variable.
+
+
+*/
+
+//creating class using valirables/
+	class A{
+		public function __construct(){
+			echo "asd";
+		}
+
+	}
+
+
+	$class = "A";
+
+	new $class();
+
+
+//-----------------------------------
+	
+
+?>
---
+++ prep.php
@@ -0,0 +1,12 @@
+<?php
+namespace hello
+
+class test{
+
+	public 
+
+}
+
+
+?>
+
